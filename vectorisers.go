7679e6ca0e974566aa37a823aa3322f6eb6c2d66
James Bowman <james.edward.bowman@gmail.com>
2017-03-15 21:28:20 +0000

    added documentation

--- vectorisers.go
+++ vectorisers.go
@@ -7,15 +7,31 @@ import (
 	"github.com/gonum/matrix/mat64"
 )
 
+// CountVectoriser can be used to encode one or more text documents into a term document 
+// matrix where each column represents a document within the corpus and each row represents
+// a term present in the training data set.  Each element represents the frequency the
+// corresponding term appears in the corresponding document e.g. tf(t, d) = 5 would mean
+// that term t (perhaps the word "dog") appears 5 times in the document d.
 type CountVectoriser struct {
+	// Vocabulary is a map of words to indices that point to the row number representing 
+	// that word in the term document matrix output from the Transform() and FitTransform() 
+	// methods.  The Vocabulary map is populated by the Fit() or FitTransform() methods 
+	// based upon the words occuring in the datasets supplied to those methods.  Within 
+	// Transform(), any words found in the test data set that were not present in the 
+	// training data set supplied to Fit() will not have an entry in the Vocabulary 
+	// and will be ignored.
 	Vocabulary    map[string]int
 	wordTokeniser *regexp.Regexp
 }
 
+// NewCountVectoriser creates a new CountVectoriser
 func NewCountVectoriser() *CountVectoriser {
 	return &CountVectoriser{Vocabulary: make(map[string]int), wordTokeniser: regexp.MustCompile("\\w+")}
 }
 
+// Fit processes the supplied training data (a variable number of strings representing 
+// documents).  Each word appearing inside the training data will be added to the 
+// Vocabulary
 func (v *CountVectoriser) Fit(train ...string) *CountVectoriser {
 	i := 0
 	for _, doc := range train {
@@ -34,7 +50,10 @@ func (v *CountVectoriser) Fit(train ...string) *CountVectoriser {
 	return v
 }
 
-// Add the document to the term document matrix under construction
+// Transform transforms the supplied documents into a term document matrix where each
+// column is a feature vector representing one of the supplied documents.  Each element
+// represents the frequency with which the associated term for that row occured within
+// that document.
 func (v *CountVectoriser) Transform(docs ...string) (*mat64.Dense, error) {
 	mat := mat64.NewDense(len(v.Vocabulary), len(docs), nil)
 
@@ -52,6 +71,9 @@ func (v *CountVectoriser) Transform(docs ...string) (*mat64.Dense, error) {
 	return mat, nil
 }
 
+// FitTransform is exactly equivalent to calling Fit() followed by Transform() on the 
+// same matrix.  This is a convenience where separate trianing data is not being 
+// used to fit the model i.e. the model is fitted on the fly to the test data.
 func (v *CountVectoriser) FitTransform(docs ...string) (*mat64.Dense, error) {
 	v.Fit(docs...)
 	return v.Transform(docs...)
--- weightings.go
+++ weightings.go
@@ -6,14 +6,27 @@ import (
 	"github.com/gonum/matrix/mat64"
 )
 
+// TfidfTransformer takes a raw term document matrix and weights each raw term frequency
+// value depending upon how commonly it occurs across all documents within the corpus.
+// For example a very commonly occuring word like `the` is likely to occur in all documents
+// and so would be weighted down.
+// More precisely, TfidfTransformer applies a tf-idf algorithm to the matrix where each
+// term frequency is multiplied by the inverse document frequency.  Inverse document 
+// frequency is calculated as log(n/df) where df is the number of documents in which the // 
+// term occurs and n is the total number of documents within the corpus.  We add 1 to both n 
+// and df before division to prevent division by zero.
 type TfidfTransformer struct {
 	transform *mat64.Dense
 }
 
+// NewTfidfTransformer constructs a new TfidfTransformer.
 func NewTfidfTransformer() *TfidfTransformer {
 	return &TfidfTransformer{}
 }
 
+// Fit takes a training term document matrix, counts term occurances across all documents
+// and constructs an inverse document frequency transform to apply to matrices in subsequent 
+// calls to Transform().
 func (t *TfidfTransformer) Fit(mat mat64.Matrix) *TfidfTransformer {
 	m, n := mat.Dims()
 
@@ -46,6 +59,9 @@ func (t *TfidfTransformer) Transform(mat mat64.Matrix) (*mat64.Dense, error) {
 	return product, nil
 }
 
+// FitTransform is exactly equivalent to calling Fit() followed by Transform() on the 
+// same matrix.  This is a convenience where separate trianing data is not being 
+// used to fit the model i.e. the model is fitted on the fly to the test data.
 func (t *TfidfTransformer) FitTransform(mat mat64.Matrix) (*mat64.Dense, error) {
 	t.Fit(mat)
 	return t.Transform(mat)
