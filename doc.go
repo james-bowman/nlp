6dd4376dac94d7860ec9c961657725970b102441
James Bowman <james.edward.bowman@gmail.com>
2017-03-16 08:36:51 +0000

    Corrected formatting

--- doc.go
+++ doc.go
@@ -7,9 +7,11 @@ The algorithms in the package typically support document input as text strings w
 
 This matrix can be manipulated through the application of additional transformations for the purpose of weighting features to remove natural biases which would skew results e.g. commonly occuring words like `the`, `of`, `and`, etc. should carry lower weight than unusual words.  Finally, the matrix is decomposed and truncated using an algorithm called SVD (`Singular Value Decomposition`) which approximates the original term document matrix with a new matrix of much lower rank (typically around 100 rather than 1000s).  This is a fundamental part of LSA and serves a number of purposes:
 
-    1. The reduced dimensionality of the data theoretically requires less memory.
-    2. As less significant dimensions are removed, there is less `noise` in the data which could have artificially skewed results.
-    3. Perhaps most importantly, the SVD effectively encodes the co-occurance of terms within the documents to capture semantic meaning rather than simply the presence (or lack of presence) of words.  This combats the problem of synonymy (a common challenge in NLP) where different words in the English language can be used to mean the same thing (synonyms).  In LSA, documents can have a high degree of semantic similarity with very few words in common.
+1. The reduced dimensionality of the data theoretically requires less memory.
+
+2. As less significant dimensions are removed, there is less `noise` in the data which could have artificially skewed results.
+
+3. Perhaps most importantly, the SVD effectively encodes the co-occurance of terms within the documents to capture semantic meaning rather than simply the presence (or lack of presence) of words.  This combats the problem of synonymy (a common challenge in NLP) where different words in the English language can be used to mean the same thing (synonyms).  In LSA, documents can have a high degree of semantic similarity with very few words in common.
 
 The post SVD matrix (with each column being a feature vector representing a document within the corpus) can be compared for similarity with each other (for clustering) or with a query (also represented as a feature vector projected into the same dimensional space).  Similarity is measured by the angle between the two feature vectors being considered.
 */
