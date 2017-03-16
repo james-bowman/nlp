e44dc49e68140825388e666b2121ccf0dc7d6071
James Bowman <james.edward.bowman@gmail.com>
2017-03-16 08:32:33 +0000

    corrected comment

--- weightings.go
+++ weightings.go
@@ -11,9 +11,9 @@ import (
 // For example a very commonly occuring word like `the` is likely to occur in all documents
 // and so would be weighted down.
 // More precisely, TfidfTransformer applies a tf-idf algorithm to the matrix where each
-// term frequency is multiplied by the inverse document frequency.  Inverse document 
-// frequency is calculated as log(n/df) where df is the number of documents in which the // 
-// term occurs and n is the total number of documents within the corpus.  We add 1 to both n 
+// term frequency is multiplied by the inverse document frequency.  Inverse document
+// frequency is calculated as log(n/df) where df is the number of documents in which the
+// term occurs and n is the total number of documents within the corpus.  We add 1 to both n
 // and df before division to prevent division by zero.
 type TfidfTransformer struct {
 	transform *mat64.Dense
@@ -25,7 +25,7 @@ func NewTfidfTransformer() *TfidfTransformer {
 }
 
 // Fit takes a training term document matrix, counts term occurances across all documents
-// and constructs an inverse document frequency transform to apply to matrices in subsequent 
+// and constructs an inverse document frequency transform to apply to matrices in subsequent
 // calls to Transform().
 func (t *TfidfTransformer) Fit(mat mat64.Matrix) *TfidfTransformer {
 	m, n := mat.Dims()
@@ -59,8 +59,8 @@ func (t *TfidfTransformer) Transform(mat mat64.Matrix) (*mat64.Dense, error) {
 	return product, nil
 }
 
-// FitTransform is exactly equivalent to calling Fit() followed by Transform() on the 
-// same matrix.  This is a convenience where separate trianing data is not being 
+// FitTransform is exactly equivalent to calling Fit() followed by Transform() on the
+// same matrix.  This is a convenience where separate trianing data is not being
 // used to fit the model i.e. the model is fitted on the fly to the test data.
 func (t *TfidfTransformer) FitTransform(mat mat64.Matrix) (*mat64.Dense, error) {
 	t.Fit(mat)
