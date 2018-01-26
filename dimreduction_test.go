1f166e6272cbb900e102e9f8ad559af615b28210
james-bowman <james.edward.bowman@gmail.com>
2018-01-26 22:29:42 +0000

    added tests for PCA

--- dimreduction_test.go
+++ dimreduction_test.go
@@ -69,6 +69,68 @@ func TestTruncatedSVDFitTransform(t *testing.T) {
 	}
 }
 
+func TestPCAFitTransform(t *testing.T) {
+	var tests = []struct {
+		m      int
+		n      int
+		input  []float64
+		k      int
+		r      int
+		c      int
+		result []float64
+	}{
+		{
+			m: 6, n: 4,
+			input: []float64{
+				1, 3, 5, 2,
+				8, 1, 0, 0,
+				2, 1, 0, 1,
+				0, 0, 0, 0,
+				0, 0, 0, 1,
+				0, 1, 0, 0,
+			},
+			k: 2,
+			r: 2, c: 4,
+			result: []float64{
+				-7.478, -0.128, 1.591, 0.496,
+				2.937, 2.581, 4.240, 1.110,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		transformer := NewPCA(test.k)
+		input := mat.NewDense(test.m, test.n, test.input)
+		expResult := mat.NewDense(test.r, test.c, test.result)
+
+		result, err := transformer.FitTransform(input)
+
+		if err != nil {
+			t.Errorf("Failed Truncated SVD transform caused by %v", err)
+		}
+
+		if !mat.EqualApprox(expResult, result, 0.01) {
+			t.Logf("Expected matrix: \n%v\n but found: \n%v\n",
+				mat.Formatted(expResult),
+				mat.Formatted(result))
+			t.Fail()
+		}
+
+		result2, err := transformer.Transform(input)
+
+		if err != nil {
+			t.Errorf("Failed Truncated SVD transform caused by %v", err)
+		}
+
+		if !mat.EqualApprox(result, result2, 0.001) {
+			t.Logf("First matrix: \n%v\n but second matrix: \n%v\n",
+				mat.Formatted(result),
+				mat.Formatted(result2))
+			t.Fail()
+		}
+	}
+}
+
 func TestTruncatedSVDSaveLoad(t *testing.T) {
 	var transforms = []struct {
 		wanted *TruncatedSVD
