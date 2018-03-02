3c813d3e2b7223fc104cbeeffe5c8c96d228c4e8
james-bowman <james.edward.bowman@gmail.com>
2018-03-02 12:52:02 +0000

    updated documentation

--- doc.go
+++ doc.go
@@ -9,6 +9,14 @@ The primary intended use case is to support document input as text strings encod
 
 These matrices can be processed and manipulated through the application of additional transformations for weighting features, identifying relationships or optimising the data for analysis, information retrieval and/or predictions.
 
+Typically the algorithms in this package implement one of two interfaces:
+	Vectoriser - Taking document input as strings and outputting matrices of numerical features.
+	Transformer - Takes matrices of numerical features and applies logic/transformation to output a new matrix.
+
+One of the implementations of Vectoriser is Pipeline which can be used to wire together pipelines composed of a Vectoriser and one or more Transformers arranged in serial so that the output from each stage forma the input of the next.  This can be used to construct a classic LSI (Latent Semantic Indexing) pipeline (vectoriser -> TF.IDF weighting -> Truncated SVD):
+
+	pipeline := nlp.NewPipeline(nlp.NewCountVectoriser(true), nlp.NewTFIDFTransformer(), nlp.NewTruncatedSVD(100))
+
 A common transformation is `TF.IDF`` for the purpose of weighting features to remove natural biases which would skew results e.g. commonly occurring words like `the`, `of`, `and`, etc. which should carry lower weight than unusual words.
 
 Term Document matrices typically have a very large number of dimensions which can cause issues with memory and performance but also skew distance/similarity measures in vector space.  Transformations are often applied to reduce the dimensionality using techniques such as Random Projection, Principal Component Analysis and Singular Value Decomposition.  These approximate the original term document matrix with a new matrix of much lower rank (typically 100s of dimensions rather than 10s or 100s of thousands).
